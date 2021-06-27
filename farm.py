fa61b661e75484388de1485924de18eaf722da3b
Charles Larson <sknnywhiteman@yahoo.com>
2021-06-27 10:38:06 -0400

    add inital connection add log function

--- farm.py
+++ farm.py
@@ -1,7 +1,34 @@
-from gpiozer import LED
-from time import sleep
+from gpiozero import LED
+import datetime
 import os
-import mysql.connector
+from mysql.connector import connect, Error
+LOGDebug = 0
+LOGWarning = 1
+LOGError = 2
 
 mysqlpass = os.environ['pass']
 led = LED(17)
+try:
+    with connect(
+        host="localhost",
+        user="root",
+        password=mysqlpass,
+        database="farm"
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
+    exit()
+
+
+def Log(level, message):
+    sql = "INSERT INTO logs (level, message, at) VALUES (" + level + ", " + \
+        message + ", " + datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S") + ");"
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        connection.commit()
+
+
+def Loop():
+    Log(LOGDebug, "Testing connection and commit")
+    exit()
