4442ef7cde0b9d7e0c0198d2f6ccf49e98ab27e7
Charlie <charlielarson15@gmail.com>
2021-06-27 16:29:29 +0100

    fix connection and log statement

--- farm.py
+++ farm.py
@@ -7,28 +7,22 @@ LOGWarning = 1
 LOGError = 2
 
 mysqlpass = os.environ['pass']
+
 led = LED(17)
-try:
-    with connect(
-        host="localhost",
-        user="root",
-        password=mysqlpass,
-        database="farm"
-    ) as connection:
-        print(connection)
-except Error as e:
-    print(e)
-    exit()
+dbConnection = connect(host="127.0.0.1", user="pi", password=mysqlpass, db="farm")
 
 
 def Log(level, message):
-    sql = "INSERT INTO logs (level, message, at) VALUES (" + level + ", " + \
-        message + ", " + datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S") + ");"
-    with connection.cursor() as cursor:
+    sql = "INSERT INTO logs (level, message, at) VALUES (" + str(level) + ", \"" + \
+        message + "\", \"" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\");"
+    print(sql)
+    with dbConnection.cursor() as cursor:
         cursor.execute(sql)
-        connection.commit()
+        dbConnection.commit()
 
 
 def Loop():
     Log(LOGDebug, "Testing connection and commit")
     exit()
+
+Loop()
