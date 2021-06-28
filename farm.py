e136b18048aa7395d01b901ba17e9511b21ccd2c
Charles Larson <sknnywhiteman@yahoo.com>
2021-06-28 01:55:07 -0400

    add temp and humidity checks

--- farm.py
+++ farm.py
@@ -3,7 +3,9 @@ from datetime import datetime
 import time
 import os
 from mysql.connector import connect
+import smbus
 import constants
+
 
 def LoadSchedule():
     sql = "SELECT at, enabled FROM schedule ORDER BY at"
@@ -33,13 +35,30 @@ def GetLightState() -> bool:
     return schedule[len(schedule) - 1]['enabled']
 
 
+def ReadTempAndHumidity():
+    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+    time.sleep(0.5)
+    data = bus.read_i2c_block_data(0x44, 0x00, 6)
+    temp = data[0] * 256 + data[1]
+    cTemp = -45 + (175 * temp / 65535.0)
+    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+    sql = "INSERT INTO readings (humidity, temperature, at) VALUES (%.2f, %.2f, %s)" % (
+        humidity, cTemp, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    print(sql)
+    with dbConnection.cursor() as cursor:
+        cursor.execute(sql)
+        dbConnection.commit()
+
+
 def Setup():
     mysqlpass = os.environ['pass']
     global power
     global dbConnection
     global schedule
+    global bus
     schedule = []
     power = LED(17)
+    bus = smbus.SMBus(1)
     dbConnection = connect(host="127.0.0.1", user="pi",
                            password=mysqlpass, db="farm")
     Log(constants.DEBUG, "Project starting up")
@@ -55,7 +74,12 @@ def Log(level, message):
 
 
 def Loop():
+    iteration = 10
     while True:
+        if iteration >= 10:
+            ReadTempAndHumidity()
+            iteration = 0
+        iteration += 1
         state = GetLightState()
         if (state and not power.is_lit):
             Log(constants.DEBUG, "Turning lights on")
@@ -64,6 +88,7 @@ def Loop():
             Log(constants.DEBUG, "Turning lights off")
             power.off()
         time.sleep(60)
+
 
 try:
     Setup()
